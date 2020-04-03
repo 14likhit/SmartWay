6b84d768a8607f2559111a92d4ea3f78c272f5b6
14likhit <likhit.u@collpoll.com>
2020-04-04 03:44:23 +0530

    Added SplashActivity.

---
+++ app/src/main/java/com/example/smartway/ui/splash/SplashActivity.java
@@ -0,0 +1,23 @@
+package com.example.smartway.ui.splash;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
+
+import android.os.Bundle;
+
+import com.example.smartway.R;
+import com.example.smartway.base.BaseActivity;
+import com.example.smartway.databinding.ActivitySplashBinding;
+
+public class SplashActivity extends BaseActivity {
+
+    public static final String TAG = "SplashActivity";
+
+    private ActivitySplashBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_splash);
+    }
+}
