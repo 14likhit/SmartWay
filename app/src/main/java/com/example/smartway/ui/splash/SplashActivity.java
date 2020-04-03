f5e890b27c7ff9e4b90a41508afa17bdda341c43
14likhit <likhit.u@collpoll.com>
2020-04-04 05:06:45 +0530

    Added timer SplashActivity.

--- app/src/main/java/com/example/smartway/ui/splash/SplashActivity.java
+++ app/src/main/java/com/example/smartway/ui/splash/SplashActivity.java
@@ -7,6 +7,7 @@ import android.os.Bundle;
 import com.example.smartway.R;
 import com.example.smartway.base.BaseActivity;
 import com.example.smartway.databinding.ActivitySplashBinding;
+import com.example.smartway.utils.ActivityLauncher;
 
 public class SplashActivity extends BaseActivity {
 
@@ -18,5 +19,28 @@ public class SplashActivity extends BaseActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         binding = DataBindingUtil.setContentView(this, R.layout.activity_splash);
+        startTimer();
     }
+
+    private void startTimer() {
+        //30 Sec Timer.
+        binding.getRoot().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+//                if (PreferenceHelper.getInstance().getUser() != null && PreferenceHelper.getInstance().getUser().getActiveUser() != null) {
+//                    if (PreferenceHelper.getInstance().getUser().getActiveUser().equals(User.USER_TYPE_TENANT)) {
+//                        ActivityLauncher.launchTenantActivity(SplashActivity.this);
+//                    } else if (PreferenceHelper.getInstance().getUser().getActiveUser().equals(User.USER_TYPE_PG_OWNER)) {
+//                        ActivityLauncher.launchPGActivity(SplashActivity.this);
+//                    } else {
+//                        ActivityLauncher.launchLoginActivity(SplashActivity.this);
+//                    }
+//                } else {
+//                    ActivityLauncher.launchLoginActivity(SplashActivity.this);
+//                }
+                ActivityLauncher.launchHomeActivity(SplashActivity.this);
+            }
+        }, 50);
+    }
+
 }
