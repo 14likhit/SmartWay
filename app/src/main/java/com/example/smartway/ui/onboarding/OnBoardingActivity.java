da06accf9ff939754d792791d201f7c4a2fb4857
14likhit <likhit.u@collpoll.com>
2020-04-04 04:14:15 +0530

    Added OnBoardingActivity and PermissionActivity. Refactored ManifestFile.

---
+++ app/src/main/java/com/example/smartway/ui/onboarding/OnBoardingActivity.java
@@ -0,0 +1,15 @@
+package com.example.smartway.ui.onboarding;
+
+import android.os.Bundle;
+
+import com.example.smartway.R;
+import com.example.smartway.base.BaseActivity;
+
+public class OnBoardingActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_on_boarding);
+    }
+}
