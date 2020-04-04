cd1cb85f384ca925d40a70506bcc7b94371549b4
14likhit <likhit.u@collpoll.com>
2020-04-04 07:50:21 +0530

    Added models Location and User.

---
+++ app/src/main/java/com/example/smartway/data/model/Location.java
@@ -0,0 +1,68 @@
+package com.example.smartway.data.model;
+
+public class Location {
+
+    private Double longitude;
+    private Double latitude;
+    private Float accuracy;
+    private String countrycode;
+    private String countryname;
+    private String locality;
+    private String postalcode;
+
+    public Double getLongitude() {
+        return longitude;
+    }
+
+    public void setLongitude(Double longitude) {
+        this.longitude = longitude;
+    }
+
+    public Double getLatitude() {
+        return latitude;
+    }
+
+    public void setLatitude(Double latitude) {
+        this.latitude = latitude;
+    }
+
+    public Float getAccuracy() {
+        return accuracy;
+    }
+
+    public void setAccuracy(Float accuracy) {
+        this.accuracy = accuracy;
+    }
+
+    public String getCountrycode() {
+        return countrycode;
+    }
+
+    public void setCountrycode(String countrycode) {
+        this.countrycode = countrycode;
+    }
+
+    public String getCountryname() {
+        return countryname;
+    }
+
+    public void setCountryname(String countryname) {
+        this.countryname = countryname;
+    }
+
+    public String getLocality() {
+        return locality;
+    }
+
+    public void setLocality(String locality) {
+        this.locality = locality;
+    }
+
+    public String getPostalcode() {
+        return postalcode;
+    }
+
+    public void setPostalcode(String postalcode) {
+        this.postalcode = postalcode;
+    }
+}
---
+++ app/src/main/java/com/example/smartway/data/model/User.java
@@ -0,0 +1,23 @@
+package com.example.smartway.data.model;
+
+public class User {
+
+    private Integer ukid;
+    private Integer phoneNumber;
+
+    public Integer getUkid() {
+        return ukid;
+    }
+
+    public void setUkid(Integer ukid) {
+        this.ukid = ukid;
+    }
+
+    public Integer getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(Integer phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+}
