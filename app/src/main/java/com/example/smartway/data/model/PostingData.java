d24e914922b23b5d3f4f6a83d0a0722bccdbd484
14likhit <likhit.u@collpoll.com>
2020-04-04 19:20:01 +0530

    Updated model classes. Added postingData pogo class.

---
+++ app/src/main/java/com/example/smartway/data/model/PostingData.java
@@ -0,0 +1,98 @@
+package com.example.smartway.data.model;
+
+public class PostingData {
+
+    private Integer id;
+    private String name;
+    private Double latitude;
+    private Double longitude;
+    private Integer deviceid;
+    private Float accuracy;
+    private String countrycode;
+    private String countryname;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Double getLatitude() {
+        return latitude;
+    }
+
+    public void setLatitude(Double latitude) {
+        this.latitude = latitude;
+    }
+
+    public Double getLongitude() {
+        return longitude;
+    }
+
+    public void setLongitude(Double longitude) {
+        this.longitude = longitude;
+    }
+
+    public Integer getDeviceid() {
+        return deviceid;
+    }
+
+    public void setDeviceid(Integer deviceid) {
+        this.deviceid = deviceid;
+    }
+
+    public Float getAccuracy() {
+        return accuracy;
+    }
+
+    public void setAccuracy(Float accuracy) {
+        this.accuracy = accuracy;
+    }
+
+    public String getCountrycode() {
+        return countrycode;
+    }
+
+    public void setCountrycode(String countrycode) {
+        this.countrycode = countrycode;
+    }
+
+    public String getCountryname() {
+        return countryname;
+    }
+
+    public void setCountryname(String countryname) {
+        this.countryname = countryname;
+    }
+
+    public String getPostalcode() {
+        return postalcode;
+    }
+
+    public void setPostalcode(String postalcode) {
+        this.postalcode = postalcode;
+    }
+
+    public String getLocality() {
+        return locality;
+    }
+
+    public void setLocality(String locality) {
+        this.locality = locality;
+    }
+
+    private String postalcode;
+    private String locality;
+
+
+}
--- app/src/main/java/com/example/smartway/data/model/User.java
+++ app/src/main/java/com/example/smartway/data/model/User.java
@@ -4,6 +4,8 @@ public class User {
 
     private Integer ukid;
     private Integer phoneNumber;
+    private String userName;
+    private Integer userDeviceId;
 
     public Integer getUkid() {
         return ukid;
@@ -19,5 +21,21 @@ public class User {
 
     public void setPhoneNumber(Integer phoneNumber) {
         this.phoneNumber = phoneNumber;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public Integer getUserDeviceId() {
+        return userDeviceId;
+    }
+
+    public void setUserDeviceId(Integer userDeviceId) {
+        this.userDeviceId = userDeviceId;
     }
 }
